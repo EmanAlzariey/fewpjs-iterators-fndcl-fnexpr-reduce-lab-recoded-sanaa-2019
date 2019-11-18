4b92918a5c81c82c01385204bff27086f3f84869
Eman Al-zariey <eman_alzariey@yahoo.com>
2019-11-18 08:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+console.log("Hello");
+
+
+let totalBatteries = batteryBatches.reduce((item,value) => item+value)
+
+
+console.log(totalBatteries);
+
+
